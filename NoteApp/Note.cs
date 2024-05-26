15697bc74fad945306754503c6b429a3d8a9da2c
MihailPod <kontrakt1996@gmail.com>
2024-05-27 04:05:12 +0700

    Созданы классы: Note, NoteCategory, Project, ProjectManager

---
+++ NoteApp/Note.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NoteApp
+{
+    /// <summary>
+    /// Класс заметок, хранящий информацию о названии, содержании,
+    /// категории, времени создании и последнего изменения.
+    /// </summary>
+    public class Note : ICloneable
+    {
+        string _title;
+        EnumNoteCategory _category;
+        string _noteText;
+        DateTime _creationTime;
+        DateTime _timeOfLastChange;
+
+        public Note(string title, EnumNoteCategory category, string noteText, DateTime creationTime, DateTime timeOfLastChange)
+        {
+            Title = title;
+            Category = category;
+            NoteText = noteText;
+            CreationTime = creationTime;
+            TimeOfLastChange = timeOfLastChange;
+        }
+
+        /// <summary>
+        /// Возвращает и задает название (не длиннее 50 символов)
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value == "")
+                {
+                    _title = "Без названия";
+                }
+                if (_title != value)
+                {
+                    if (value.Length < 50)
+                    {
+                        _title = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Название не может быть длинее 50 символов!");
+                    }
+                }
+                _timeOfLastChange = DateTime.Now;
+            }
+        }
+        /// <summary>
+        /// Возвращает и задает категорию согласно списку категорий
+        /// </summary>
+        public EnumNoteCategory Category
+        {
+            get { return _category; }
+            set
+            {
+                if (value == EnumNoteCategory.Job || value == EnumNoteCategory.Home
+                || value == EnumNoteCategory.HealthAndSports || value == EnumNoteCategory.People
+                || value == EnumNoteCategory.Documents || value == EnumNoteCategory.Finance
+                || value == EnumNoteCategory.Others)
+                {
+                    _category = value;
+                    _timeOfLastChange = DateTime.Now;
+                }
+                else
+                {
+                    throw new ArgumentException("Категория должна быть выбрана из списка");
+                }
+            }
+        }
+        /// <summary>
+        /// Возвращает и задает текст заметки
+        /// </summary>
+        public string NoteText
+        {
+            get { return _noteText; }
+            set
+            {
+                if (_noteText != value)
+                {
+                    if (value != "")
+                    {
+                        _noteText = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Заметка не может быть пустой!");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Возвращает время создания
+        /// </summary>
+        public DateTime CreationTime
+        {
+            get { return _creationTime; }
+            set
+            {
+                if (_creationTime != value)
+                {
+                    if (value != DateTime.MinValue)
+                    {
+                        _creationTime = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Некорректная дата!");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Возвращает время последнего изменения
+        /// </summary>
+        public DateTime TimeOfLastChange
+        {
+            get { return _timeOfLastChange; }
+            set
+            {
+                if (_timeOfLastChange != value)
+                {
+                    if (value != DateTime.MinValue)
+                    {
+                        _timeOfLastChange = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Некорректная дата!");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Метод клонирования заметки
+        /// </summary>
+        /// <returns> Возвращает новую, эдентичную старой заметку</returns>
+        public object Clone()
+        {
+            return new Note(Title, Category, NoteText, CreationTime, TimeOfLastChange);
+        }
+    }
+}
---
+++ NoteApp/Project.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    public class Project
+    {
+        private List<Note> _notes;
+        public List<Note> Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+        public Project(List<Note> notes)
+        {
+            Notes = notes;
+        }
+        public class NoteComparer : IComparer<Note>
+        {
+            public int Compare(Note x, Note y)
+            {
+                return y.TimeOfLastChange.CompareTo(x.TimeOfLastChange);
+            }
+        }
+
+        public List<Note> GetSortedNotes(int selectedCategory)
+        {
+            List<Note> notes = new List<Note>();
+            var noteCategorys = this.Notes.ToArray();
+            for (int i = 0; i < noteCategorys.Length; i++)
+            {
+                int x = (int)noteCategorys[i].Category;
+                if (x == selectedCategory - 1)
+                    notes.Add(noteCategorys[i]);
+            }
+            notes.Sort(new NoteComparer());
+            return notes;
+        }
+
+        private Note _currentNote;
+        public Note CurrentNote
+        {
+            get { return _currentNote; }
+            set { _currentNote = value; }
+        }
+    }
+}
